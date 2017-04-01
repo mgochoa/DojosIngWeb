9355f8f9bd47d5797370899f7cd9e4122277d6f7
Manuel Ochoa <manuel.ochoa@aliadosweb.com>
2017-04-01 11:13:35 -0500

    Dojo vue

---
+++ README.md
@@ -0,0 +1,9 @@
+# DOJOS INGENIERIA WEB
+----
+
+## Universidad de Antiouia
+## 2017-1
+
+### Manuel Ochoa Loaiza
+
+### 1036739399
---
+++ Vue-js/src/components/Contenido.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div class="">
+    <div class="cabecera">
+      <p>{{email.asunto}}</p>
+      <p>FROM: {{email.from}}</p>
+      <p>TO: {{email.to}}</p>
+    </div>
+    <div class="contenido">
+      <p>{{email.contenido}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    email: ""
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+  .cabecera{
+    padding: 30px;
+    border-radius: 30px;
+    background-color: #F2F2F2;
+  }
+
+  .contenido{
+    padding: 30px;
+  }
+</style>
